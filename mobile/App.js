c37de64686bda370393acfaac56421919dc4901a
cfasilva <cleberfasilva@gmail.com>
2020-03-28 00:57:12 -0300

    clear/orde layout.

--- mobile/App.js
+++ mobile/App.js
@@ -1,22 +1,10 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>
-        Te Amo!
-        Com ♥️ Cleber
-      </Text>
+    <View>
+      <Text>Hello World</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#7159c1',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
